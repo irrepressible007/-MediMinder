013484c9145bfa986b2ed0b6749fe8fcaeaceb6f
irrepressible007 <ahamzs362@gmail.com>
2026-07-01 00:50:22 +0600

    Initial commit: Project setup, dependencies, and Compose Theme

---
+++ app/src/main/java/com/example/mediminder/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.mediminder.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenPrimaryDark = Color(0xFF4ADE80)
+val BlueSecondaryDark = Color(0xFF60A5FA)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val ErrorDark = Color(0xFFF87171)
+val OnPrimaryDark = Color(0xFF000000)
+
+val GreenPrimaryLight = Color(0xFF16A34A)
+val BlueSecondaryLight = Color(0xFF2563EB)
+val BackgroundLight = Color(0xFFF8FAFC)
+val SurfaceLight = Color(0xFFFFFFFF)
+val ErrorLight = Color(0xFFDC2626)
+val OnPrimaryLight = Color(0xFFFFFFFF)
